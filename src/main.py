e542888a0e7827d4f872302f610e47bc0dd08055
devanamul <ahfahad118@gmail.com>
2026-08-29 15:07:36 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from datetime import date
 
+from utils import add, subtract
+
 if __name__ == "__main__":
     print("Anamul Hasan")
     print(date.today().isoformat())
+    print(add(5, 3))
+    print(subtract(5, 3))
